9169af6258bdad3becf56007bb59a5c7a169df62
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-08-20 15:04:26 -0300

    Adicionando botões de alarme

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 //expo install @react-native-picker/picker
 import {Picker} from '@react-native-picker/picker';
+//expo install expo-linear-gradient
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
 
@@ -11,14 +13,28 @@ export default function App() {
 
   const [alarmeSound, setAlarmeSound] = useState([
     {
+      id:1,
       selecionado: true,
       som: 'alarme 1',
       file: 'alarm1.mp3',
     },
     {
+      id:2,
       selecionado: false,
       som: 'alarme 2',
       file: 'alarm2.mp3',
+    },
+    {
+      id:3,
+      selecionado: false,
+      som: 'alarme 3',
+      file: 'alarm3.mp3',
+    },
+    {
+      id:4,
+      selecionado: false,
+      som: 'alarme 4',
+      file: 'alarm4.mp3',
     },
   ]);
 
@@ -29,12 +45,34 @@ export default function App() {
     numeros.push(i);
   }
 
+  function setarAlarmeSound(id){
+    let alarmesTemp = alarmeSound.map(function(val){
+      if(id != val.id){
+        val.selecionado = false;
+      } else{
+        val.selecionado = true;
+      }
+      return val;
+    });
+    setAlarmeSound(alarmesTemp);
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <LinearGradient
+        //Gradiente de fundo
+        colors={['rgba(50,50,50,0.7)', 'rgba(0,0,0,0.9)']}
+        style={{
+          position: 'absolute',
+          left:0,
+          right: 0,
+          top: 0,
+          height: '100%',
+        }} />
       <Text style={styles.textoSelcione}>Selecione o seu Tempo: </Text>
       <View style={styles.timeContainer}>
-        <Text style={{color:'white'}}>Min:</Text>
+        <Text style={{color:'white', paddingTop:16}}>Min:</Text>
         <Picker
           selectedValue={minutos}
           style={{height: 50, width: 100, color:'white'}}
@@ -47,7 +85,7 @@ export default function App() {
               })
             }
         </Picker>
-        <Text style={{color:'white'}}>Seg:</Text>
+        <Text style={{color:'white', paddingTop:16}}>Seg:</Text>
         <Picker
           selectedValue={segundos}
           style={{height: 50, width: 100, color:'white'}}
@@ -62,6 +100,29 @@ export default function App() {
             }
         </Picker>
       </View>
+
+      <View style={{flexDirection:'row', flexWrap:'wrap'}}>
+        {
+          alarmeSound.map(function(val){
+            if(val.selecionado){
+              return(
+                <TouchableOpacity 
+                  style={styles.btnSelecionado}
+                  onPress={() => setarAlarmeSound(val.id)} >
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>{val.som}</Text>
+                </TouchableOpacity>
+              );
+            } else {
+              return(
+                <TouchableOpacity onPress={() => setarAlarmeSound(val.id)} style={styles.btnEscolher} >
+                  <Text style={{color: 'white', fontWeight: 'bold'}}>{val.som}</Text>
+                </TouchableOpacity>
+              );
+            }
+          })
+        }
+      </View>
+
     </View>
   );
 }
@@ -69,7 +130,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgb(85,55,175)',
+    //backgroundColor: 'rgb(85,55,175)',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -80,5 +141,21 @@ const styles = StyleSheet.create({
   },
   timeContainer: {
     flexDirection:"row",
+  },
+  btnEscolher:{
+    margin: 6,
+    paddingVertical: 15,
+    paddingHorizontal: 65,
+    backgroundColor: 'rgb(80,80,80)',
+    borderRadius: 30,
+  },
+  btnSelecionado:{
+    margin: 6,
+    paddingVertical: 15,
+    paddingHorizontal: 65,
+    backgroundColor: 'rgba(80,80,80,0.25)',
+    borderRadius: 30,
+    borderColor: 'white',
+    borderWidth: 1.5,
   }
 });
